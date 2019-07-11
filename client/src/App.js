aef2faa787838d775e9c2c2063103fa292c6dbd9
Mvcampbell3 <mvcampbell3@gmail.com>
2019-07-11 16:11:16 -0400

    added in basic router and pages in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import Home from "./pages/Home"
+import Lost from "./pages/Lost"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the starter MERN app</h1>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact render={props => <Home />} />
+          <Route component={Lost} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ client/src/pages/Lost/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./lost.css"
+
+const Lost = (props) => {
+  return ( 
+    <div>
+      <h1>This is the 404 page</h1>
+    </div>
+   );
+}
+ 
+export default Lost;
