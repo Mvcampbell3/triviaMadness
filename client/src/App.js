50bc4324ddd4f448fadb47646496dfaf203dd198
Mvcampbell3 <mvcampbell3@gmail.com>
2019-07-12 11:50:56 -0400

    added in login page with state and funcitons in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,50 @@ import "./App.css";
 
 import Home from "./pages/Home"
 import Lost from "./pages/Lost"
+import Login from "./pages/Login"
 
 class App extends Component {
+  state = {
+    user: false,
+    username: "",
+    email: "",
+    password: "",
+    signup: false
+  }
+
+  loginUser = () => {
+
+  }
+
+  signupUser = () => {
+
+  }
+
+  checkAuth = () => {
+
+  }
+
+  switchSignup = () => {
+    this.setState({ signup: !this.state.signup })
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/" exact render={props => <Home />} />
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact render={props => <Login
+            email={this.state.email}
+            password={this.state.password}
+            username={this.state.username}
+            signup={this.state.signup}
+            switchSignup={this.switchSignup}
+            handleInputChange={this.handleInputChange}
+          />} />
           <Route component={Lost} />
         </Switch>
       </Router>
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Login = (props) => {
+  return (
+    <div className="loginBox">
+      <div className="loginForm">
+        <div className="loginGroup">
+          <label>Email</label>
+          <input type="email" name="email" autoComplete="off" onChange={e => props.handleInputChange(e)} value={props.email} />
+        </div>
+        {/* Need to hide username input group on login */}
+        <div className="loginGroup">
+          <label>Username</label>
+          <input type="text" name="username" autoComplete="off" onChange={e => props.handleInputChange(e)} value={props.username} />
+        </div>
+        <div className="loginGroup">
+          <label>Password</label>
+          <input type="password" name="password" autoComplete="off" onChange={e => props.handleInputChange(e)} value={props.password} />
+        </div>
+      </div>
+      <div className="loginButtons">
+        {props.signup ? <button>Sign Up</button>:<button>Login</button>}
+        <button className="switchSignup" onClick={props.switchSignup}>{props.signup ? "Already a member?":"Not a member yet?"}</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
