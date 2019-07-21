4b3c7564cab8858937764d170c8500cae5c45ef1
Mvcampbell3 <mvcampbell3@gmail.com>
2019-07-20 21:25:06 -0400

    added gameSelection styling

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -8,20 +8,8 @@ const Home = (props) => {
       <Header user={props.user} logoutUser={props.logoutUser} />
       <div className="wrapper">
         {props.resetSendHome()}
-        <h1>This is the Home Page</h1>
-        <h4>Welcome to trivia madness</h4>
-        <p>This site is for people who love to play and create trivia games!</p>
-        <p>There are going to be different categories of quizzes</p>
-        <ul style={{ listStylePosition: "inside" }}>
-          <li>History</li>
-          <li>Science</li>
-          <li>Movies and TV</li>
-          <li>Music</li>
-          <li>Other</li>
-        </ul>
-        <p>If we get enough 'Other' games with the same kind of category, we will add it to the selection list</p>
-        <p>I am not sure what to do about images, we might just have one for each type of category</p>
-        <p>We could also set up the background to have different ones based on category</p>
+        <h1>This is the Home page</h1>
+        <p className="content">We will be adding in all of the most important stuff very soon</p>
       </div>
     </div>
   );
